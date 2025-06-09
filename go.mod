a27434accd408ffa58a649dbe522c021d2f90fed
Nicomnr02 <nicolasraja@synapsis.id>
2025-06-09 23:50:31 +0700

    feat: init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module nicotheowedding
+
+go 1.24.3
+
+require (
+	github.com/aws/aws-sdk-go v1.55.7
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("No .env file found (falling back to system env vars)")
+	}
+
+	http.HandleFunc("/preview", serveSignedURL)
+	fmt.Println("Server started at :2300")
+	log.Fatal(http.ListenAndServe(":2300", nil))
+}
+
+func serveSignedURL(w http.ResponseWriter, r *http.Request) {
+
+	accessKey := os.Getenv("ACCESS_KEY")
+	secretKey := os.Getenv("SECRET_KEY")
+	bucket := "nico-wedding"
+	object := r.URL.Query().Get("object")
+
+	// Initialize AWS S3 client but point it to GCS
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region:           aws.String("auto"), // region is ignored but required
+		Endpoint:         aws.String("https://storage.googleapis.com"),
+		S3ForcePathStyle: aws.Bool(true),
+		Credentials:      credentials.NewStaticCredentials(accessKey, secretKey, ""),
+	}))
+
+	svc := s3.New(sess)
+
+	// Set expiration time
+	req, _ := svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(object),
+	})
+
+	// Generate pre-signed URL
+	url, err := req.Presign(20 * time.Minute)
+	if err != nil {
+		log.Fatalf("Failed to sign request: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, `{"signed_url":"%s"}`, url)
+}
